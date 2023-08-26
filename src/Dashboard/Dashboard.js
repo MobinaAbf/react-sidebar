9d2cbc00a9f1d6c3fa051dbba49236bbd099a537
Mobina Abforoosh <mobinaabf@gmail.com>
2023-08-26 12:59:44 +0330

    add logo image and btn for close

--- src/Dashboard/Dashboard.js
+++ src/Dashboard/Dashboard.js
@@ -1,9 +1,21 @@
 import './Dashboard.css';
+import myImage from './maze-logo-removebg-preview.png'; // Import your image file
+
 
 const Dashboard = () => {
   return (
     <div className="container">
-        
+        <aside>
+            <div className='toggle'>
+                <div className='logo'>
+                <img src={myImage} alt="My-img" />
+                <h2>Dashboard</h2>
+                </div>
+                <div className='close' id='close-btn'>
+
+                </div>
+            </div>
+        </aside>
     </div> 
   );
 }
