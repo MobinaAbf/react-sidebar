3b0ecfe865e8aebf05d47b6c23c226f6aa5a159a
Mobina Abforoosh <mobinaabf@gmail.com>
2023-08-26 08:38:24 +0330

    create react app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Loginform from './Loginform/Loginform';
+import React from 'react';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Loginform/>
+    </div>
+  );
+}
+
+export default App;
