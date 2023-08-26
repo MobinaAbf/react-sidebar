6dfe46e8814cc137f536c22b27dca1907a89d5d9
Mobina Abforoosh <mobinaabf@gmail.com>
2023-08-26 09:03:15 +0330

    create Dashboard function in the Dashboard.js

--- src/Dashboard/Dashboard.js
+++ src/Dashboard/Dashboard.js
@@ -1,2 +1,11 @@
+import './Dashboard.css';
 
+const Dashboard = () => {
+  return (
+    <div className="container">
+        <h2>hiiiiiiiiiiiiiiii</h2>
+    </div> 
+  );
+}
 
+export default Dashboard;
